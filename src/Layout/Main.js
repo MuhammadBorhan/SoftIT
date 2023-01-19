8e0ba0dac8f16ef6542cc6bfe0851e152b0d34ea
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-01-19 16:21:48 +0600

    so far so good

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -11,34 +11,35 @@ const Main = () => {
     });
   });
   return (
-    <div>
+    <>
       <header
         className={`${
-          bg ? "bg-slate-900 h-20" : "h-20"
+          bg ? "bg-indigo-700 h-20" : "h-20"
         } flex items-center fixed top-0 w-full text-white bg-black z-10 transition-all duration-300`}
       >
         <div className="container mx-auto h-full flex items-center justify-between">
           {/* logo */}
-          <Link to="/">
-            <h1 className="text-4xl font-bold">
-              <span>Soft</span>
-              <span className="text-yellow-400">IT</span>
-            </h1>
+          <Link to="/" className="text-4xl font-bold">
+            <span>Soft</span>
+            <span className="text-yellow-400">IT</span>
           </Link>
+
           {/* navbar for desktop*/}
           <div className="hidden lg:block">
             <Navbar />
           </div>
+
           {/* navbar for mobile*/}
           {/* <div className="lg:hidden">
             <NavMobile />
           </div> */}
         </div>
       </header>
+
       <div>
         <Outlet />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div className="mt-20 ">This is about</div>;
+  return <div className="mt-20">This is about</div>;
 };
 
 export default About;
