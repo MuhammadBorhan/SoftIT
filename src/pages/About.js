ab781dafd1ce0b4b87f820c6d3ea780c47d95511
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-01-19 23:14:09 +0600

    title changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { ChangeTitle } from "../utils/ChangeTitle";
 
 const About = () => {
+  ChangeTitle("About");
   return <div className="mt-20 h-[800px]">This is about</div>;
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Slider from "../components/slider/Slider";
+import { ChangeTitle } from "../utils/ChangeTitle";
 
 const Home = () => {
+  ChangeTitle("Home");
   return (
-    <div className="mt-20">
+    <div className="mt-20 lg:mt-0">
       <Slider />
     </div>
   );
